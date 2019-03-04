3d27b6b450473ac41fd81124252b6374d11a6a1f
Jneko <Jneko@gmail.com>
2019-03-04 11:57:38 +0100

    Añadimos un nuevo metodo (factorialRecursiva) a la clase Operacions.

--- src/github/Operacions.java
+++ src/github/Operacions.java
@@ -15,4 +15,15 @@ public class Operacions {
         }
         return aux;
     }
+	
+	public double factorial_recursiva(int n){
+        if(n < 0){
+            throw  new IllegalArgumentException();
+        }
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        Factorial fmenos1 = new Factorial();
+        return n * fmenos1.getRecursiva(n - 1);
+    }
 }
